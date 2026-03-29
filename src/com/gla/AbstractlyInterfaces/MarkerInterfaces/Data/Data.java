86b5354d544c80e1be5cb605c6e402a2b23ca146
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-03-29 22:42:50 +0530

    added

---
+++ src/com/gla/AbstractlyInterfaces/MarkerInterfaces/Data/Data.java
@@ -0,0 +1,9 @@
+package com.gla.AbstractlyInterfaces.MarkerInterfaces.Data;
+
+class UserData implements BackupSerializable {
+    String name;
+
+    UserData(String name) {
+        this.name = name;
+    }
+}
