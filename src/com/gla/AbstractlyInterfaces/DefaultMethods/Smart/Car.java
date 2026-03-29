909533c1ad4499bdc9f2ca4961cd70a6334dfaac
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-03-29 22:43:14 +0530

    added

---
+++ src/com/gla/AbstractlyInterfaces/DefaultMethods/Smart/Car.java
@@ -0,0 +1,8 @@
+package com.gla.AbstractlyInterfaces.DefaultMethods.Smart;
+
+class Car implements VehicleDashboard {
+
+    public void displaySpeed() {
+        System.out.println("Car Speed: 80 km/h");
+    }
+}
