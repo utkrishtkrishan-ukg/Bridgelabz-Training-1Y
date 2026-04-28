7f4f8f67a53684d8f3b0669e21bdcaa74eb49264
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:13:49 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/string_api/level2/MaximumOfThree.java
@@ -0,0 +1,25 @@
+package com.gla.Objects_classes.level2;
+import java.util.Scanner;
+public class MaximumOfThree {
+    static int findMax(int a, int b, int c) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        System.out.println("Maximum number is: " + findMax(a, b, c));
+    }
+}
