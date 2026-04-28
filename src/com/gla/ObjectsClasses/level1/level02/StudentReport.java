d7f54e41e61d909d7820426bbc1b5bbf89eabe6c
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:20:16 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/ObjectsClasses/level1/level02/StudentReport.java
@@ -0,0 +1,55 @@
+package com.gla.ObjectsClasses.level1.level02;
+import java.util.Scanner;
+
+class Student {
+    String name;
+    int rollNumber;
+    double marks;
+
+    void setDetails(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    String calculateGrade() {
+        if (marks >= 90)
+            return "A";
+        else if (marks >= 75)
+            return "B";
+        else if (marks >= 60)
+            return "C";
+        else if (marks >= 40)
+            return "D";
+        else
+            return "Fail";
+    }
+
+    void displayDetails() {
+        System.out.println("\nStudent Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+}
+
+public class StudentReport {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Roll Number: ");
+        int roll = sc.nextInt();
+
+        System.out.print("Enter Marks: ");
+        double marks = sc.nextDouble();
+
+        Student s = new Student();
+        s.setDetails(name, roll, marks);
+        s.displayDetails();
+    }
+}
+
