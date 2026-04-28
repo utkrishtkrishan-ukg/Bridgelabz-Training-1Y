a1f718e843faf77037949ad390c7d26069437152
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:09:17 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/Package/EmployeeManagement/Employee.java
@@ -0,0 +1,16 @@
+package com.gla.Package.EmployeeManagement;
+public class Employee {
+    private int id;
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(int id, String name, String dept, double salary) {
+        this.id=id; this.name=name; this.department=dept; this.salary=salary;
+    }
+
+    public double getSalary(){ return salary; }
+    public void setSalary(double salary){ this.salary=salary; }
+
+    public String getName(){ return name; }
+}
