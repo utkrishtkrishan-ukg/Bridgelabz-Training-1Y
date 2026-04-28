506c6e1453f62be4629c89cd1e4a8bf11129d269
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:20:26 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/arrays/ArrayTraversal.java
@@ -0,0 +1,13 @@
+package com.gla.arrays;
+
+public class ArrayTraversal {
+    public static void main(String [] args){
+
+        int[] ar = {3,4,5,6,7,8};
+
+        for(int i = 0; i < ar.length; i++){
+            System.out.println(i+" ");
+            }
+        }
+    }
+
