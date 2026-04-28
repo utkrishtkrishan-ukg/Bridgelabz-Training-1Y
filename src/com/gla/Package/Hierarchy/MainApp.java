d2ba8ab954df758cd2f1152a0f585f5ff9b3ed12
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:09:16 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/Package/Hierarchy/MainApp.java
@@ -0,0 +1,9 @@
+package com.gla.Package.Hierarchy;
+import com.gla.Package.Hierarchy.Course;
+public class MainApp {
+    public static void main(String[] args) {
+        Course c = new Course();
+        c.input();
+        c.display();
+    }
+}
