598186694ff56342cac5b3c623946c997b9920bd
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:14:37 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/string_api/EXTRASassignment/PalindromeCheck.java
@@ -0,0 +1,26 @@
+package com.gla.Objects_classes.EXTRASassignment;
+import java.util.*;
+public class PalindromeCheck {
+    public static boolean isPalindrome(String str) {
+        int start = 0, end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        if (isPalindrome(str))
+            System.out.println("It is a Palindrome.");
+        else
+            System.out.println("It is not a Palindrome.");
+    }
+}
