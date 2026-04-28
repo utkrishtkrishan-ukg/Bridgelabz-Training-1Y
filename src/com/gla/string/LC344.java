800694312487dbd5824a9a03fff9a7bf95b137e6
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:19:49 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/string/LC344.java
@@ -0,0 +1,19 @@
+package com.gla.string;
+
+public class LC344 {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
+
+    static void main() {
+        
+    }
+}
