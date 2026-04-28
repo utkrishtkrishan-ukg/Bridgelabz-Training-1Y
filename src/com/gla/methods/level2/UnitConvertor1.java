1baaed29cbf126a1a8f319fbe417fb9db4debc5f
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:09:52 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/methods/level2/UnitConvertor1.java
@@ -0,0 +1,23 @@
+package com.gla.methods.level2;
+
+public class UnitConvertor1 {
+    public static double convertKmToMiles(double km) {
+        double km2miles = 0.621371;
+        return km * km2miles;
+    }
+
+    public static double convertMilesToKm(double miles) {
+        double miles2km = 1.60934;
+        return miles * miles2km;
+    }
+
+    public static double convertMetersToFeet(double meters) {
+        double meters2feet = 3.28084;
+        return meters * meters2feet;
+    }
+
+    public static double convertFeetToMeters(double feet) {
+        double feet2meters = 0.3048;
+        return feet * feet2meters;
+    }
+}
