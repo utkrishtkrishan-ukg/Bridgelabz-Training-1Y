9c92729bf0c677243958be2ff510188aa1c876ae
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:09:04 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/Package/ques1/college/MainApp.java
@@ -0,0 +1,18 @@
+package com.gla.Package.ques1.college;
+
+/*import college.student.Student;
+import college.faculty.Faculty;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.input();
+        s.display();
+
+        Faculty f = new Faculty();
+        f.input();
+        f.display();
+    }
+}
+
+ */
