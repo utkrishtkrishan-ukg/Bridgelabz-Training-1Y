1ee1238bb7cab1978b25f8022fd3fcfcf555d0eb
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:09:13 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/Package/library/Transaction.java
@@ -0,0 +1,6 @@
+package com.gla.Package.library;
+public class Transaction {
+    public void issue(String book, String member) {
+        System.out.println(member + " issued book: " + book);
+    }
+}
