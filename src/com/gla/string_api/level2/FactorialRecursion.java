26b841e56581fe19f78cccf37b412e01e0d96590
Utkrisht krishan <utkrisht.krishan_cs25@gla.ac.in>
2026-04-28 11:13:44 +0530

    Uploaded via GitDrop

---
+++ src/com/gla/string_api/level2/FactorialRecursion.java
@@ -0,0 +1,18 @@
+package com.gla.Objects_classes.level2;
+import java.util.Scanner;
+public class FactorialRecursion {
+    static long factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("Factorial is: " + factorial(num));
+    }
+}
